06d94b0b132843e3ca70ee0cd4bfe5be9370332d
Benantar Abdennour <ha_benantar@esi.dz>
2021-11-25 21:02:22 +0100

    modified requirements

--- main.py
+++ main.py
@@ -42,3 +42,7 @@ def parse_string_to_list(value:str):
     for v in value_items:
         value_list.append(v)
     return value_list
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
